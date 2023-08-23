b6feae6aab3930351491f47ed7cdd89a37f31a70
Hyunny <hazel413@naver.com>
2023-08-24 08:51:48 +0900

    [ADD]common files(html, css, js)

---
+++ js/common.js
@@ -0,0 +1,29 @@
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
+
+
+//datepicker
+$( "#datepicker" ).datepicker();
+
+
+//드롭다운
+$('.selected-option').click(function () {
+  $('.options-list').toggle();
+  // $('.custom-dropdown').css({ 'border-bottom-left-radius': '0px;',
+  //   'border-bottom-right-radius': '0px;'})
+});
+
+$('.options-list li').click(function () {
+  var selectedValue = $(this).data('value');
+  $('.selected-option').text($(this).text());
+  $('.options-list').hide();
+});
+
+$('.delete-button').click(function (event) {
+  event.stopPropagation(); // 이벤트 버블링 방지
+  $(this).parent().remove();
+});
