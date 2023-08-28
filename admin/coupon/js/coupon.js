26be3e744670635ad385c2460e64c72ce7205a5c
Jelly <110519070+stars2302@users.noreply.github.com>
2023-08-28 21:02:10 +0900

    [FEAT] coupon_create 마무리 코드 추가

--- admin/coupon/js/coupon.js
+++ admin/coupon/js/coupon.js
@@ -1,3 +1,13 @@
 $('.thumb_btn').click(function(){
   $('.thumbnail .hidden').trigger('click');
 });
+
+$('.coupon_sale_check, .coupon_limit_date').find('input').change(function(){
+  let $this = $(this);
+  console.log($this);
+  if($(this).prop('checked')){
+    $('input').prop('disabled',false);
+    $this.parent().siblings('.form-check').find('.input').prop('disabled',true);
+  }
+});
+$('.coupon_sale_check, .coupon_limit_date').find('input').trigger('change');
