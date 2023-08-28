60397bc1ee3406b2ab35ba422e04161416036984
HYEIN-SEP <hyeinpark22@gmail.com>
2023-08-29 01:23:42 +0900

    [FIX]강의등록 html,css 위계 수정

--- admin/course/js/course.js
+++ admin/course/js/course.js
@@ -1,34 +1,3 @@
 $("#product_detail").summernote({
   height: 400,
 });
-
-// $(".form-select").selectmenu();
-
-var uploadFiles = [];
-var $drop = $("#drop");
-$drop
-  .on("dragenter", function (e) {
-    //드래그 요소가 들어왔을떄
-    $(this).addClass("drag-enter");
-  })
-  .on("dragleave", function (e) {
-    //드래그 요소가 나갔을때
-    $(this).removeClass("drag-enter");
-  })
-  .on("dragover", function (e) {
-    e.stopPropagation();
-    e.preventDefault();
-  })
-  .on("drop", function (e) {
-    //드래그한 항목을 떨어뜨렸을때
-    e.preventDefault();
-    console.log(e);
-
-    $(this).removeClass("drag-enter");
-    var files = e.originalEvent.dataTransfer.files; //드래그&드랍 항목
-    console.log(files);
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
-      attachFile(file);
-    }
-  });
