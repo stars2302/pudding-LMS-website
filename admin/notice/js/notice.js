f7008814d1f7a417e97f922d5189d90aef83309e
jungsuk chae <portfoliochae@gmail.com>
2023-08-29 17:17:35 +0900

    [FIX] 공지사항 내용 변경

--- admin/notice/js/notice.js
+++ admin/notice/js/notice.js
@@ -20,4 +20,30 @@ $('.btn_modify').click(function(){
 });
 
 /* notice_create.js */
-$('#summernote').summernote();
+$('#summernote').summernote({
+  /* 폰트선택 툴바 사용하려면 주석해제 */
+  // fontNames: ['Roboto Light', 'Roboto Regular', 'Roboto Bold', 'Apple SD Gothic Neo'],
+  // fontNamesIgnoreCheck: ['Apple SD Gothic Neo'],
+  placeholder: '공지사항 내용을 입력해 주세요',
+  tabsize: 2,
+  height: 150,
+  resize: false,
+  lang: "ko-KR",
+  disableResizeEditor: true,
+  toolbar: [
+      /* 폰트선택 툴바 사용하려면 주석해제 */
+      // ['fontname', ['fontname']],
+      ['fontsize', ['fontsize']],
+      ['style', ['bold', 'italic', 'underline', 'clear']],
+      ['color', ['color']],
+      ['table', ['table']],
+      ['para', ['paragraph']],
+      ['insert', ['link', 'picture']],
+      ['view', []]
+  ],
+  callbacks: {	//여기 부분이 이미지를 첨부하는 부분
+      onImageUpload: function (files) {
+          RealTimeImageUpdate(files, this);
+      }
+  }
+});
