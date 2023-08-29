1ab63748dda5c43ced78cac391d027ae0b5eaded
stars2302 <stars2302@naver.com>
2023-08-29 17:32:20 +0900

    [FEAT] coupon input 활성화/비활성화 script 추가

--- admin/coupon/js/coupon.js
+++ admin/coupon/js/coupon.js
@@ -2,6 +2,7 @@ $('.thumb_btn').click(function(){
   $('.thumbnail .hidden').trigger('click');
 });
 
+/*
 $('.coupon_sale_check, .coupon_limit_date').find('input').change(function(){
   let $this = $(this);
   console.log($this);
@@ -11,3 +12,25 @@ $('.coupon_sale_check, .coupon_limit_date').find('input').change(function(){
   }
 });
 $('.coupon_sale_check, .coupon_limit_date').find('input').trigger('change');
+*/
+
+$('.coupon_sale_check').change(function(){
+  let $this = $(this);
+  // inputDisable($this);
+  if($this.find('input[type="radio"]').filter(':checked')){
+    $this.parent().find('.input').prop('disabled',true);
+    $this.find('input[type="radio"]:checked').siblings('.input').prop('disabled',false);
+    console.log($this.parent().find('.input'));
+    console.log($this.find('input[type="radio"]:checked').siblings('.input'));
+  }
+});
+$('.coupon_sale_check').trigger('change');
+
+$('.coupon_limit_date').change(function(){
+  $this = $(this);
+  if(!$(this).find('input[name="coupon_limit_date"]').prop('checked')){
+    $this.find('.form-select').prop('disabled',false);
+  } else{
+    $this.find('.form-select').prop('disabled',true);
+  }
+});
