420c33ff795815c968156140e60936c6f3a67fd0
Hyunny <hazel413@naver.com>
2023-08-30 15:53:15 +0900

    [FEAT]sidebar php

--- admin/inc/footer.php
+++ admin/inc/footer.php
@@ -27,7 +27,9 @@
       src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"
       referrerpolicy="no-referrer"
     ></script>
-
+<!-- chart js -->
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> 
     <script src="js/common.js"></script>
+    <script src="js/index.js"></script>
   </body>
 </html>
--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,12 @@
 <?php
  
   include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/admin/inc/header.php';
+
+?>
+    <link rel="stylesheet" href="/pudding-LMS-website/admin/css/index.css" />
+<?php
+ include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/admin/inc/sidebar.php';
+
 ?>
         
         <section class="dashboard_wrap">
@@ -11,7 +17,7 @@
                 <ul class="sales_lank d-flex gap-4 justify-content-center align-content-center">
                   <li>
                     <div class="d-flex flex-column">
-                      <img src="/admin/images/course/thumbnail_7.png" alt="강의 이미지">
+                      <img src="/pudding-LMS-website/admin/images/course/thumbnail_7.png" alt="강의 이미지">
                       <span class="badge rounded-pill red_bg b-pd">1위</span>
                       <h3>도전! HTML, CSS</h3>
                       <p>8월 매출액 : 8,000,000원</p>
@@ -19,7 +25,7 @@
                   </li>
                   <li>
                     <div class="d-flex flex-column">
-                      <img src="/admin/images/course/thumbnail_8.png" alt="강의 이미지">
+                      <img src="/pudding-LMS-website/admin/images/course/thumbnail_8.png" alt="강의 이미지">
                       <span class="badge rounded-pill blue_bg b-pd">2위</span>
                       <h3>Javascript의 이해</h3>
                       <p>8월 매출액 : 6,000,000원</p>
@@ -27,7 +33,7 @@
                   </li>
                   <li>
                     <div class="d-flex flex-column">
-                      <img src="/admin/images/course/thumbnail_3.png" alt="강의 이미지">
+                      <img src="/pudding-LMS-website/admin/images/course/thumbnail_3.png" alt="강의 이미지">
                       <span class="badge rounded-pill green_bg b-pd">3위</span>
                       <h3>UX/UI 마스터</h3>
                       <p>8월 매출액 : 3,000,000원</p>
@@ -79,51 +85,7 @@
       </div><!-- content_wrap -->
     </div><!-- wrap -->
   
-    <!-- jquery -->
-    <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
-    integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
-    crossorigin="anonymous"
-    referrerpolicy="no-referrer"
-  ></script>
-  <!-- jqueryui js -->
-  <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.13.2/jquery-ui.min.js"
-    integrity="sha512-57oZ/vW8ANMjR/KQ6Be9v/+/h6bq9/l3f0Oc7vn6qMqyhvPd1cvKBRWWpzu0QoneImqr2SkmO4MSqU+RpHom3Q=="
-    crossorigin="anonymous"
-    referrerpolicy="no-referrer"
-  ></script>
-  <!-- bootstrap js -->
-  <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/js/bootstrap.min.js"
-    integrity="sha512-fHY2UiQlipUq0dEabSM4s+phmn+bcxSYzXP4vAXItBvBHU7zAM/mkhCZjtBEIJexhOMzZbgFlPLuErlJF2b+0g=="
-    crossorigin="anonymous"
-    referrerpolicy="no-referrer"
-  ></script>
-  <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/js/bootstrap.bundle.min.js"
-    integrity="sha512-ToL6UYWePxjhDQKNioSi4AyJ5KkRxY+F1+Fi7Jgh0Hp5Kk2/s8FD7zusJDdonfe5B00Qw+B8taXxF6CFLnqNCw=="
-    crossorigin="anonymous"
-    referrerpolicy="no-referrer"
-  ></script>
-  <!-- fontawesome js  -->
-  <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"
-    integrity="sha512-uKQ39gEGiyUJl4AI6L+ekBdGKpGw4xJ55+xyJG7YFlJokPNYegn9KwQ3P8A7aFQAUtUsAQHep+d/lrGqrbPIDQ=="
-    crossorigin="anonymous"
-    referrerpolicy="no-referrer"
-  ></script>
-  <!-- modernizr js -->
-  <script
-    src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"
-    referrerpolicy="no-referrer"
-  ></script>
-<!-- chart js -->
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>  <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script> -->
-  <!-- tabler-icons js -->
-  <script src="node_modules/@tabler/icons/dist/umd/tabler-icons.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/jquery-ui.min.js"></script>
-  <script src="js/index.js"></script>
-  </body>
-  </html>
+<?php
+
+ include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/admin/inc/footer.php';
+?>
