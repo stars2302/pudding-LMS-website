934ba2d6e476e97ce997f97356acce53c4c5e6f9
Jelly <110519070+stars2302@users.noreply.github.com>
2023-08-31 21:04:22 +0900

    [FEAT] coupon_create 쿠폰 등록(완성)

--- admin/coupon/js/coupon.js
+++ admin/coupon/js/coupon.js
@@ -1,68 +1,53 @@
 
-//create 사진등록 button click하면 숨어있던 input[type="file"]실행
 $('.thumb_btn').click(function(){
+  //create 사진등록 button click하면 숨어있던 input[type="file"]실행
   $('.thumbnail .hidden').trigger('click');
 });
 
-
-$('.thumbnail .hidden').change(function(e){
-  console.log(e.target.files[0].name);
-  let fileSRC = e.target.files[0].name;
+$('#thumbnail').change(function(){
   console.log($(this).val());
-  attachFile(fileSRC);
+  let file = $(this).prop('files');
+  console.log(file);
+
+    let formData = new FormData(); //페이지 전환없이 이페이지 바로 이미지 등록
+    formData.append('savefile', file[0]); //<input type="file" name="cp_image" value="파일명">
+    $.ajax({
+      url: 'coupon_image_save.php',
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false,
+      dataType: 'json',
+      type: 'POST',
+      error: function (error) {
+        console.log('error:', error)
+      },
+      success: function (return_data) {
+        if(return_data.result == 'image'){
+          alert('이미지파일만 첨부할 수 있습니다.');
+          return;
+        } else if(return_data.result == 'size'){
+          alert('10MB 이하만 첨부할 수 있습니다.');
+          return;
+        } else if(return_data.result == 'error'){
+          alert('관리자에게 문의하세요');
+          return;
+        } else{
+          $('.show_thumb').html(`<img src="${return_data.src}" alt="">`);
+        }
+      }
+
+    });//ajax
 });
 
-function attachFile(file) {
-  console.log(file);
-  let formData = new FormData(); //페이지 전환없이 이페이지 바로 이미지 등록
-  formData.append('savefile', file) //<input type="file" name="savefile" value="파일명">
-  console.log(formData);
-  /*
-  $.ajax({
-    url: 'coupon_image_save.php',
-    data: formData,
-    cache: false,
-    contentType: false,
-    processData: false,
-    dataType: 'json',
-    type: 'POST',
-    error: function (error) {
-      console.log('error:', error)
-    },
-    success: function (return_data) {
+//쿠폰등록 이미지 validate
+$('.coupon_submit_btn').click(function(){
+  console.log($('#thumbnail').val());
+  if(!$('#thumbnail').val()){
+    alert('쿠폰 이미지를 등록해주세요');
+  }
+});
 
-      console.log(return_data);
-
-      // if (return_data.result == 'member') {
-      //   alert('로그인을 하십시오.');
-      //   return;
-      // } 
-      if (return_data.result == 'image') {
-        alert('이미지파일만 첨부할 수 있습니다.');
-        return;
-      } else if (return_data.result == 'size') {
-        alert('10메가 이하만 첨부할 수 있습니다.');
-        return;
-      } else if (return_data.result == 'error') {
-        alert('관리자에게 문의하세요');
-        return;
-      } else {
-        //첨부이미지 테이블에 저장하면 할일
-        let imgid = $('#file_table_id').val() + return_data.imgid + ',';
-        $('#file_table_id').val(imgid);
-        let html = `
-            <div class="thumb" id="f_${return_data.imgid}" data-imgid="${return_data.imgid}">
-              <img src="/abcmall/pdata/${return_data.savefile}" alt="">
-              <button type="button" class="btn btn-warning">삭제</button>
-            </div>
-        `;
-        $('#thumbnails').append(html);
-      }
-    }
-
-  });//ajax
-  */
-} //func attachFile
 
 
 
@@ -72,9 +57,10 @@ $('.coupon_sale_check').change(function(){
   // inputDisable($this);
   if($this.find('input[type="radio"]').filter(':checked')){
     $this.parent().find('.input').prop('disabled',true);
+    $this.parent().find('.input').prop('required',false);
     $this.find('input[type="radio"]:checked').siblings('.input').prop('disabled',false);
-    console.log($this.parent().find('.input'));
-    console.log($this.find('input[type="radio"]:checked').siblings('.input'));
+    $this.find('input[type="radio"]:checked').siblings('.input').prop('required',true);
+    $this.find('input[type="radio"]:not(:checked)').siblings('.input').val('');//radio input 바꾸면 val 없애기
   }
 });
 $('.coupon_sale_check').trigger('change');
