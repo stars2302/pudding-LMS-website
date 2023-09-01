16238163d12ad1eaf3b3bdb6ab36a46c3875eeb8
Jelly <110519070+stars2302@users.noreply.github.com>
2023-09-01 18:11:20 +0900

    [FEAT] coupon_list 데이터 출력 및 쿠폰명 여백 변경

--- admin/coupon/coupon_list.php
+++ admin/coupon/coupon_list.php
@@ -3,6 +3,14 @@ $title="쿠폰리스트";
 $css_route="coupon/css/coupon_list.css";
 $js_route = "coupon/js/coupon.js";
 include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/admin/inc/header.php';
+
+$sql = "SELECT * from coupons order by cpid desc";
+$result = $mysqli -> query($sql);
+while($rs = $result -> fetch_object()){
+  $rsc[] = $rs;
+}
+
+// var_dump($rsc);
 ?>
 
     <div class="sub_header d-flex justify-content-between align-items-center">
@@ -23,17 +31,18 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/admin/inc/header.ph
 
     <div class="coupon_filter">
       <h2 class="hidden">클릭하여 진행중 또는 종료된 쿠폰을 확인하세요.</h2>
-      <form action="" class="coupon_status_search d-flex justify-content-betweenn white_bg align-items-center">
+      <form action="" class="coupon_status_search d-flex justify-content-betweenn white_bg align-items-center" method="POST">
+        <input type="hidden" name="filter" value="" id="coupon_filter_val">
         <div class="filter_1">
           <h3 class="b_text01">총 쿠폰 개수</h3>
           <button class="b_text02"><em class="main_tt">10</em>개</button>
         </div>
         <div class="filter_2">
-          <h3 class="b_text01">진행중 쿠폰 개수</h3>
+          <h3 class="b_text01">활성화 쿠폰 개수</h3>
           <button class="b_text02"><em class="main_tt">6</em>개</button>
         </div>
         <div class="filter_3">
-          <h3 class="b_text01">종료된 쿠폰 개수</h3>
+          <h3 class="b_text01">비활성화 쿠폰 개수</h3>
           <button class="b_text02"><em class="main_tt">4</em>개</button>
         </div>
       </form>
@@ -42,168 +51,56 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/admin/inc/header.ph
     <div class="coupons">
       <h2 class="hidden">쿠폰리스트</h2>
       <ul class="d-flex flex-wrap justify-content-between g-5">
-        <li class="coupon shadow_box border white_bg d-flex">
-          <img src="images/coupon_1.jpg" alt="" class="border">
+        <?php
+        if(isset($rsc)){
+          foreach($rsc as $coupon){
+        ?>
+
+        <li class="coupon shadow_box border white_bg d-flex" data-cpid="<?= $coupon->cpid ?>">
+          <img src="<?= $coupon->cp_image ?>" alt="<?= $coupon->cp_name ?>" class="border">
           <div class="text_box">
-            <span class="badge rounded-pill badge_blue b-pd">진행중</span>
-            <h3 class="b_text01"><a href="#">회원가입 축하 쿠폰</a></h3>
-            <p>사용기한 : 무제한</p>
-            <p>최소사용금액 : 50,000원</p>
-            <p>할인율 : 10%</p>
+            <?php
+            if($coupon->cp_status == 1){
+              echo '<span class="badge rounded-pill badge_blue b-pd">활성화</span>';
+            } else{
+              echo '<span class="badge rounded-pill badge_red b-pd">비활성화</span>';
+            }
+            ?>
+            <h3 class="b_text01"><?= $coupon->cp_name ?></h3>
+            <p>사용기한 : <?php if($coupon->cp_date == ''){echo '무제한';}else{echo $coupon->cp_date.'개월';} ?></p>
+            <p>최소사용금액 : <?= $coupon->cp_limit ?>원</p>
+            <p class="number"><?php if($coupon->cp_type == '정액'){echo '할인액';}else{echo '할인율';} ?> : <?php if($coupon->cp_type == '정액'){echo $coupon->cp_price.'원';}else{echo $coupon->cp_ratio.'%';} ?></p>
           </div>
 
           <div class="icons">
-            <i class="ti ti-edit pen_icon"></i>
+            <a href="/pudding-LMS-website/admin/coupon/coupon_update.php?cpid=<?= $coupon->cpid ?>"><i class="ti ti-edit pen_icon"></i></a>
             <i class="ti ti-trash bin_icon"></i>
           </div>
 
-          <div class="form-check form-switch">
+          <div class="form-check form-switch cp_status_toggle">
             <input
               class="form-check-input"
               type="checkbox"
               role="switch"
               id="flexSwitchCheckDefault"
+              <?php if($coupon->cp_status == 1){echo 'checked';} else{echo '';}?>
             />
-            <label class="form-check-label" for="flexSwitchCheckDefault"
-              ></label
-            >
+            <label class="form-check-label" for="flexSwitchCheckDefault">
+            </label>
           </div>
         </li>
-        <li class="coupon shadow_box border white_bg d-flex">
-          <img src="images/coupon_1.jpg" alt="" class="border">
-          <div class="text_box">
-            <span class="badge rounded-pill badge_blue b-pd">진행중</span>
-            <h3 class="b_text01">회원가입 축하 쿠폰</h3>
-            <p>사용기한 : 무제한</p>
-            <p>최소사용금액 : 50,000원</p>
-            <p>할인율 : 10%</p>
-          </div>
 
-          <div class="icons">
-            <i class="ti ti-edit pen_icon"></i>
-            <i class="ti ti-trash bin_icon"></i>
-          </div>
+        <?php
+          }
+        }else {
+        ?>
 
-          <div class="form-check form-switch">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-            />
-            <label class="form-check-label" for="flexSwitchCheckDefault"
-              ></label
-            >
-          </div>
-        </li>
-        <li class="coupon shadow_box border white_bg d-flex">
-          <img src="images/coupon_1.jpg" alt="" class="border">
-          <div class="text_box">
-            <span class="badge rounded-pill badge_blue b-pd">진행중</span>
-            <h3 class="b_text01">회원가입 축하 쿠폰</h3>
-            <p>사용기한 : 무제한</p>
-            <p>최소사용금액 : 50,000원</p>
-            <p>할인율 : 10%</p>
-          </div>
+        
+        <p>등록된 쿠폰이 없습니다.</p>
 
-          <div class="icons">
-            <i class="ti ti-edit pen_icon"></i>
-            <i class="ti ti-trash bin_icon"></i>
-          </div>
-
-          <div class="form-check form-switch">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-            />
-            <label class="form-check-label" for="flexSwitchCheckDefault"
-              ></label
-            >
-          </div>
-        </li>
-        <li class="coupon shadow_box border white_bg d-flex">
-          <img src="images/coupon_1.jpg" alt="" class="border">
-          <div class="text_box">
-            <span class="badge rounded-pill badge_blue b-pd">진행중</span>
-            <h3 class="b_text01">회원가입 축하 쿠폰</h3>
-            <p>사용기한 : 무제한</p>
-            <p>최소사용금액 : 50,000원</p>
-            <p>할인율 : 10%</p>
-          </div>
-
-          <div class="icons">
-            <i class="ti ti-edit pen_icon"></i>
-            <i class="ti ti-trash bin_icon"></i>
-          </div>
-
-          <div class="form-check form-switch">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-            />
-            <label class="form-check-label" for="flexSwitchCheckDefault"
-              ></label
-            >
-          </div>
-        </li>
-        <li class="coupon shadow_box border white_bg d-flex">
-          <img src="images/coupon_1.jpg" alt="" class="border">
-          <div class="text_box">
-            <span class="badge rounded-pill badge_blue b-pd">진행중</span>
-            <h3 class="b_text01">회원가입 축하 쿠폰</h3>
-            <p>사용기한 : 무제한</p>
-            <p>최소사용금액 : 50,000원</p>
-            <p>할인율 : 10%</p>
-          </div>
-
-          <div class="icons">
-            <i class="ti ti-edit pen_icon"></i>
-            <i class="ti ti-trash bin_icon"></i>
-          </div>
-
-          <div class="form-check form-switch">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-            />
-            <label class="form-check-label" for="flexSwitchCheckDefault"
-              ></label
-            >
-          </div>
-        </li>
-        <li class="coupon shadow_box border white_bg d-flex">
-          <img src="images/coupon_1.jpg" alt="" class="border">
-          <div class="text_box">
-            <span class="badge rounded-pill badge_blue b-pd">진행중</span>
-            <h3 class="b_text01">회원가입 축하 쿠폰</h3>
-            <p>사용기한 : 무제한</p>
-            <p>최소사용금액 : 50,000원</p>
-            <p>할인율 : 10%</p>
-          </div>
-
-          <div class="icons">
-            <i class="ti ti-edit pen_icon"></i>
-            <i class="ti ti-trash bin_icon"></i>
-          </div>
-
-          <div class="form-check form-switch">
-            <input
-              class="form-check-input"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-            />
-            <label class="form-check-label" for="flexSwitchCheckDefault"
-              ></label
-            >
-          </div>
-        </li>
+        <?php
+        }
+        ?>
       </ul>
     </div>
 
