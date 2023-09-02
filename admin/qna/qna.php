b8a35e48a600d813917cebc1eabeaa1c0cb79446
teastz <137160482+teastz@users.noreply.github.com>
2023-09-02 16:16:15 +0900

    [FIX] 테스트 php 코드 추가

--- admin/qna/qna.php
+++ admin/qna/qna.php
@@ -7,16 +7,44 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/pudding-LMS-website/admin/inc/dbcon.p
 include_once $_SERVER['DOCUMENT_ROOT'] . '/pudding-LMS-website/admin/inc/header.php';
 
 $pagenationTarget = 'qna';
-include_once $_SERVER['DOCUMENT_ROOT'].'/abcmall/admin/inc/pagenation.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/pudding-LMS-website/admin/qna/qna_page.php';
+
+
+$mysqli = new mysqli($hostname, $dbuserid, $dbpasswd, $dbname);
+
+
+
+$pagesql = "SELECT COUNT(*) as pid from qna";
+$page_result = $mysqli->query($pagesql);
+$page_row = $page_result->fetch_assoc();
+//print_r($page_row['cnt']);
+$row_num = $page_row['pid']; //전체 게시물 수
+
+$list = 10; //페이지당 출력할 게시물 수
+$block_ct = 5;
+// $block_num = ceil($page/$block_ct);     //page9,  9/5 1.2 2
+$block_start = (($block_num -1)*$block_ct) + 1;//page6 start 6
+$block_end = $block_start + $block_ct -1; //start 1, end 5
+
+$total_page = ceil($row_num/$list); //총42, 42/5
+if($block_end > $total_page) $block_end = $total_page;
+$total_block = ceil($total_page/$block_ct);//총32, 2
+
+// $start_num = ($page -1) * $list;
 
 ?>
 
 <?php
 
-$search = isset($_GET['search']) ? $_GET['search'] : ''; //search
-
+if ($mysqli->connect_error) {
+  die("연결 실패" . $mysqli->connect_error);
+}
 ?>
 
+
+<?php
+$search = isset($_GET['search']) ? $_GET['search'] : ''; //search
+?>
 
 <section>
         <h2 class="main_tt">Q&amp;A 게시판</h2>
@@ -40,13 +68,31 @@ $search = isset($_GET['search']) ? $_GET['search'] : ''; //search
           </thead>
 
           <tbody class="b_text01">
+           
+          <?php
+          
+          $sql = "SELECT * FROM qna order by pid desc limit 0, 10";
+          // $result = $mysqli->query($sql)
+
+          // while($row = mysqli_fetch_array($result, MYSQLI_ASSOC);){
+ 
+          ?>
+          
             <tr>
-              <th scope="row">답변대기</th>
-              <td>HTML은 어떤건가요?</td>
-              <td>사용자</td>
-              <td>2023-08-22</td>
-              <td>2</td>
+              <th><?= $row['dix']; ?></th>
+              <td><?= $row['q_title']; ?></td>
+              <td><?= $row['q_regdate']; ?></td>
+              <td><?= $row['uid']; ?></td>
+              <td><?= $row['dix']; ?></td>
             </tr>
+          
+            <?php
+        
+            // }
+
+            ?>
+            
+          
             <tr>
               <th scope="row">답변대기</th>
               <td>HTML은 어떤건가요?</td>
@@ -76,6 +122,9 @@ $search = isset($_GET['search']) ? $_GET['search'] : ''; //search
             </tr>
           </tbody>
         </table>
+            <div class="btns">
+              <a href="">글쓰기 테스트</a>
+            </div>
 
 
         <nav
