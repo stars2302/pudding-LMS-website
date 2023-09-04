b382c5c574e889215f9959ba3df4a5e3aa324a94
Hyunny <hazel413@naver.com>
2023-09-04 22:02:21 +0900

    [FEAT]sales page 추가, chartjs, 차트 받기, 월 선택

--- admin/sales/sales.php
+++ admin/sales/sales.php
@@ -43,6 +43,43 @@ if (isset($_POST['month'])) {
 
 // var_dump($rsc);
 
+//bar_chart
+// 선택한 월에 해당하는 데이터를 가져오는 SQL 쿼리 작성
+$sql = "SELECT name, SUM(total_price) AS total_price_sum
+        FROM payments
+        WHERE DATE_FORMAT(buy_date, '%m') = '$selected_month'
+        GROUP BY name
+        ORDER BY total_price_sum DESC";
+
+$result = $mysqli->query($sql);
+$chartData = [];
+
+while ($row = $result->fetch_assoc()) {
+    $chartData[] = [
+        'name' => $row['name'],
+        'total_price_sum' => $row['total_price_sum'],
+    ];
+}
+
+//pie
+// 선택한 7월에 가장 많은 금액을 구매한 사용자를 검색하는 SQL 쿼리
+$sqlTopBuyers = "SELECT userid, SUM(total_price) AS total_price_sum
+                FROM payments
+                WHERE DATE_FORMAT(buy_date, '%m') = '07'
+                GROUP BY userid
+                ORDER BY total_price_sum DESC
+                LIMIT 5"; // 상위 5명만 가져오도록 제한
+
+$resultTopBuyers = $mysqli->query($sqlTopBuyers);
+$topBuyersData = [];
+
+while ($row = $resultTopBuyers->fetch_assoc()) {
+    $topBuyersData[] = [
+        'userid' => $row['userid'],
+        'total_price_sum' => $row['total_price_sum'],
+    ];
+}
+
 ?>
 <!-- top_bar -->
       
@@ -50,8 +87,8 @@ if (isset($_POST['month'])) {
         <h2 class="main_tt dark">월별매출통계</h2>
     
         <form action="" id="search_form" method="POST">
-        <div class="row">
-            <div class="col-md-2">
+        <div >
+            <div class>
                 <select class="form-select cate_select" aria-label="Default select example" id="month" name="month">
                     <?php
                     foreach ($months as $key => $value) {
@@ -66,7 +103,9 @@ if (isset($_POST['month'])) {
     </form>
           <div class="d-flex justify-content-between chart">
             <div class="chart_container shadow_box">
-              <canvas id="line-chart"></canvas>
+              <canvas id="pie_chart"></canvas>
+             
+
             </div>
             <div class="chart_container shadow_box">
               <canvas id="bar_chart"></canvas>
@@ -135,6 +174,69 @@ if (isset($_POST['month'])) {
 <script src="/pudding-LMS-website/admin/course/js/makeoption.js"></script>
 
 <script>
+// PHP데이터-> JavaScript
+var chartData = <?php echo json_encode($chartData); ?>;
+var chartLabels = chartData.map(item => item.name);
+var chartValues = chartData.map(item => item.total_price_sum);
+
+// 막대 그래프 생성
+var ctx = document.getElementById('bar_chart').getContext('2d');
+var barChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: chartLabels,
+        datasets: [{
+            label: '매출',
+            data: chartValues,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+
+// 파이 그래프 데이터 생성
+var pieChartLabels = <?php echo json_encode(array_column($topBuyersData, 'userid')); ?>;
+var pieChartValues = <?php echo json_encode(array_column($topBuyersData, 'total_price_sum')); ?>;
+
+// 파이 그래프 생성
+var ctxPie = document.getElementById('pie_chart').getContext('2d');
+var pieChart = new Chart(ctxPie, {
+    type: 'pie',
+    data: {
+        labels: pieChartLabels,
+        datasets: [{
+            data: pieChartValues,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.7)',
+                'rgba(54, 162, 235, 0.7)',
+                'rgba(255, 206, 86, 0.7)',
+                'rgba(75, 192, 192, 0.7)',
+                'rgba(153, 102, 255, 0.7)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+    }
+});
 
 
 
