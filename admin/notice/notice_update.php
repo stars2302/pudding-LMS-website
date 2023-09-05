8b4a3da609eebc1308a4d5f7759f549aff1c9128
rhslfmafma <portfoliochae@gmail.com>
2023-09-05 21:37:15 +0900

    [FIX] 공지사항 내용 수정중

--- admin/notice/notice_update.php
+++ admin/notice/notice_update.php
@@ -9,10 +9,31 @@ $sql = "SELECT * FROM notice WHERE ntid='{$ntid}'";
 $result = $mysqli->query($sql);
 $sqlarr = $result -> fetch_object();
 
+    //파일 업로드
+    $save_dir = $_SERVER['DOCUMENT_ROOT'] . "/pudding-LMS-website/admin/images/notice/";
+    $filename = $_FILES['nt_filename']['name']; //insta.jpg
+    $ext = pathinfo($filename, PATHINFO_EXTENSION); //jpg
+    $upload_path = "/pudding-LMS-website/admin/images/notice/" . $filename; //파일 경로
+    $newfilename = date("YmdHis") . substr(rand(), 0, 6); //20238171184015
+    $thumbnail = $newfilename . "." . $ext; //20238171184015.jpg
+
+
+
+    if (move_uploaded_file($_FILES['nt_filename']['tmp_name'], $save_dir . $thumbnail)) {
+      $upload_option_image = "/pudding-LMS-website/admin/images/notice/" . $thumbnail;
+
+  } else {
+    echo "<script>
+        alert('이미지등록 실패!');    
+        // history.back();            
+      </script>";
+  }
+
 ?>
 <section>
 <h2 class="main_tt">공지사항 수정</h2>
-<form class="notice_create_form" action="notice_update_ok.php?ntid=<?= $ntid; ?>" method="POST">
+<form class="notice_create_form" action="notice_update_ok.php" method="POST" enctype="multipart/form-data">
+  <input type="hidden" name="ntid" value="<?= $sqlarr -> ntid; ?>">
   <div class="notice_create_form_div">
     <h3 class="content_tt"><label for="title">제목</label></h3>
     <input type="text" id="title" name="nt_title" class="notice_create_input form-control" placeholder="" aria-label="Username" 
