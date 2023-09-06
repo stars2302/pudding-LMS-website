a46ad0aff3700e2e6c98d7be5d596e1fc9ca1e96
Hyunny <hazel413@naver.com>
2023-09-06 10:08:54 +0900

    [FIX]header.php에서 경로 바꾸기

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -101,7 +101,7 @@
                 >
               </li>
               <li>
-                <a href="/pudding-LMS-website/admin/sales/sales.php" class="icon"
+                <a href="/pudding-LMS-website/admin/sales/sales_list.php" class="icon"
                   ><i class="ti ti-coins"></i><span>매출 관리</span></a
                 >
               </li>
