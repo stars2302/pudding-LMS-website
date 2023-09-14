7daae0a14823d819cb45af238b2558aff1dffcc8
HYEIN-SEP <hyeinpark22@gmail.com>
2023-09-14 17:59:44 +0900

    [ADD]user_course_view.html/css/js 리스트랑 수강평 추가

---
+++ user/js/user_course.js
@@ -0,0 +1,13 @@
+$('.viewWrap_2').hide();
+$('.viewB_1').click(function(){
+  $('.viewWrap_2').hide();
+  $('.viewWrap_1').show();
+  $('.viewB_1').addClass('active');
+  $('.viewB_2').removeClass('active');
+})
+$('.viewB_2').click(function(){
+  $('.viewWrap_1').hide();
+  $('.viewWrap_2').show();
+  $('.viewB_2').addClass('active');
+  $('.viewB_1').removeClass('active');
+})
