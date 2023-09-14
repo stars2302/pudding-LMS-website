4585e3fb6a87ff7e2d825d24e0ce65ffe7724486
yurimeeee <137126594+yurimeeee@users.noreply.github.com>
2023-09-14 16:39:56 +0900

    [FAET] footer html, css 수정

--- user/js/common.js
+++ user/js/common.js
@@ -1,31 +1,22 @@
+// top, history btn
 let topBtn = $('.top_btn');
 let historyBtn = $('.history_btn');
 
-$(window).on("scroll",function (e) {
+$(window).on("scroll", function(e) {
   e.preventDefault();
   let scrollPosition = $(this).scrollTop();
-  //let footerOST = $("footer").offset().top;
- 
 
-  if (scrollPosition > 500 ) {
+  if (scrollPosition > 500) {
     topBtn.addClass("active");
     historyBtn.addClass("active");
-  }else{
+  } else {
     topBtn.removeClass("active");
     historyBtn.removeClass("active");
   }
-
-  topBtn.on("click",function (e) {
+  topBtn.on("click", function(e) {
     e.preventDefault();
-    $("html, body").stop().animate({ scrollTop: 0 }, "easeInCubic");
+    $("html, body").stop().animate({
+      scrollTop: 0
+    }, "easeInCubic");
   });
-  
-  
-  // if(scrollPosition >= footerOST - 500) {
-  //   goTop.addClass("light");
-  //   logoBtn.addClass("light");
-  // }else{
-  // goTop.removeClass("light");
-  // logoBtn.removeClass("light");
-  // }
 });
