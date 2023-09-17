e45916bf0fed77f5d2f2d8d9908b47565c726458
Jelly <110519070+stars2302@users.noreply.github.com>
2023-09-17 19:29:46 +0900

    [ADD] 결제완료 페이지 추가

---
+++ user/cart/cart_complete.php
@@ -0,0 +1,16 @@
+<?php
+$title="장바구니";
+$css_route="cart/css/cart.css";
+$js_route = "cart/js/cart.js";
+include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/user/inc/header.php';
+?>
+
+
+<h2 class="text-center mt-5">결제완료!</h2>
+<h4 class="text-center mt-5">오늘도 PUDDING을 이용해주셔서 감사합니다</h4>
+
+
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/user/inc/footer.php';
+?>
