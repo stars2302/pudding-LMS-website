d95bc1a2ffaa9c3b7e5255c25b63195dc78d791b
Jelly <110519070+stars2302@users.noreply.github.com>
2023-09-18 16:11:33 +0900

    [FEAT] 결제완료 페이지 디벨롭

--- user/cart/cart_complete.php
+++ user/cart/cart_complete.php
@@ -6,8 +6,12 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/user/inc/header.php
 ?>
 
 
-<h2 class="text-center mt-5">결제완료!</h2>
-<h4 class="text-center mt-5">오늘도 PUDDING을 이용해주셔서 감사합니다</h4>
+<div class="cart_complete shadow_box container radius_12 white_bg text-center d-flex flex-column justify-content-center align-items-center">
+  <h2 class="main_tt">결제완료!</h2>
+  <p class="b_text01">PUDDING을 이용해주셔서 감사합니다</p>
+  <img src="images/cart_1.png" alt="">
+  <a href="/pudding-LMS-website/user/index.php" class="btn btn-primary dark mt-3">강의보러가기</a>
+</div>
 
 
 <?php
