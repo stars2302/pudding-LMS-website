9af7df6fc0284d9c1b0621518d3b4c3c18d922ad
Jelly <110519070+stars2302@users.noreply.github.com>
2023-09-18 16:12:34 +0900

    [ADD] 오늘의 쿠폰 게임 페이지 추가

---
+++ user/banner/banner.php
@@ -0,0 +1,16 @@
+<?php
+$title="오늘의쿠폰";
+$css_route="banner/css/banner.css";
+$js_route = "banner/js/banner.js";
+include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/user/inc/header.php';
+?>
+
+
+
+
+
+
+
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/user/inc/footer.php';
+?>
