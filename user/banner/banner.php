a324690eb59c9ad2e6b5068d781b4591047175e5
Jelly <110519070+stars2302@users.noreply.github.com>
2023-09-19 16:31:29 +0900

    [FEAT] banner 쿠폰 게임(진행중)

--- user/banner/banner.php
+++ user/banner/banner.php
@@ -6,7 +6,20 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/pudding-LMS-website/user/inc/header.php
 ?>
 
 
-
+<div class="container d-flex justify-content-center flex-column align-items-center game_start_container">
+  <div class="game_start text-center jua col-6 white_bg radius_12 shadow_box">
+    <h2>
+      <span class="star main_tt">★★★</span><br>
+      WORDLE 게임 
+    </h2>
+    <p class="main_stt">
+      게임 풀고 강의 할인 받자!<br>
+      10,000원 할인 쿠폰 증정
+    </p>
+    <a href="/pudding-LMS-website/user/banner/banner_game.php" class="btn btn-dark content_tt">game start</a>
+  </div>
+  <img src="images/pudding_1.png" alt="" class="col-4">
+</div>
 
 
 
---
+++ user/banner/js/banner.js
@@ -0,0 +1,94 @@
+let keyidx = 0;
+    let col = 1;
+    let answear = 'iloveu'.toLowerCase(); //정답!!
+    let gameCount = 6; //게임 횟수
+    let wordDOM ='';
+
+    // console.log(answear.length*gameCount)
+
+
+    //정답 길이에 따라 바둑판 만들기
+    for(let i = 1; i <= answear.length*gameCount; i++){
+      wordDOM += `<div class="word word${i}"><span></span></div>`;
+    }
+    $('.game_board').append(wordDOM).css({gridTemplateColumns: `repeat(${answear.length},100px)`});
+
+
+
+
+    let pushtext = [];
+    let pushIDX = [];
+    $('body').keydown(function(e){
+      // console.log(e.key);
+      if(col <= gameCount){
+
+        let key = e.key; //입력한 문자열
+        if (key.match(/^[a-zA-Z]$/)) {
+          // console.log('영어');
+          $('.word').eq(keyidx).find('span').text(key);
+          keyidx++;
+
+
+
+          if (keyidx % answear.length == 0) {
+            pushtext = [];
+            pushIDX = [];
+            $('.word').each(function(){
+              let idx = $(this).index();
+              if($(this).index() < col*answear.length && $(this).index() >= (col-1)*answear.length){
+                pushtext.push($('.word').eq(idx).text());
+                pushIDX.push(idx);
+              }
+            });
+            
+  
+            
+            //입력한 값 모두 소문자
+            $.each(pushtext,function(idx,val){
+              var $this = $(this);
+              pushtext[idx] = val.toLowerCase();
+  
+  
+              if(pushtext[idx] == answear.charAt(idx)){
+                $('.word').eq(pushIDX[idx]).addClass('o');
+              } else if(answear.includes(pushtext[idx])){
+                $('.word').eq(pushIDX[idx]).addClass('v');
+                // console.log('문자만');
+              } else{
+                $('.word').eq(pushIDX[idx]).addClass('x');
+                // console.log('틀렸워');
+              }
+            });
+  
+            setTimeout(() => {
+            if(pushtext.join('') == answear){
+                alert('정답입니다!');
+                location.reload();
+              }
+            }, 100);
+  
+            col++;
+          }
+        }
+        
+        //backspace
+        if(key =='Backspace'){
+          // console.log('back');
+          
+          
+          if(keyidx > 0){
+            if (keyidx % answear.length !== 0) {
+              $('.word').eq(keyidx - 1).text('');
+              keyidx--;
+            }
+          }
+        }
+        console.log(keyidx);
+        console.log(col*answear.length);
+
+
+    } else{
+      alert('GAME OVER');
+      location.reload();
+    }
+    });
