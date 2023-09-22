5d05dc0c482627f7a775a65c46cf89f795b0f5d7
Jelly <110519070+stars2302@users.noreply.github.com>
2023-09-22 09:51:41 +0900

    [FEAT] 장바구니 담기 php 중복담기제한기능추가

--- user/members/add_cart.php
+++ user/members/add_cart.php
@@ -7,15 +7,26 @@ if (isset($_SESSION['UID'])) {
   $uid = $_SESSION['UID'];
   $cid = $_GET['cid'];
 
-  $sql = "INSERT INTO cart (cid, userid) VALUES ('{$cid}', '{$uid}')";
-  $result = $mysqli->query($sql);
+  $sqluc = "SELECT * FROM cart where cid=$cid and userid='$uid'";
+  $result3 = $mysqli -> query($sqluc);
+  while($rs = $result3->fetch_object()){
+    $rscuc[]=$rs;
+  }
 
-  if (isset($result)) {
-    echo '<script>alert("장바구니 담기 성공");
-          history.back();
-          </script>';
-  } else {
-    echo "<script>history.back();</script>";
+  if(!isset($rscuc)){
+
+    $sql = "INSERT INTO cart (cid, userid) VALUES ('{$cid}', '{$uid}')";
+    $result = $mysqli->query($sql);
+  
+    if (isset($result)) {
+      echo '<script>alert("장바구니 담기 성공");
+            history.back();
+            </script>';
+    } else {
+      echo "<script>history.back();</script>";
+    }
+  } else{
+    echo "<script>alert('이미 장바구니에 담겨있습니다.');history.back();</script>";
   }
 } else {
   //UID 없다면, 로그인 페이지로 이동
